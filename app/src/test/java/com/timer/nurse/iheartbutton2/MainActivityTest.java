b59fa67f47903b152199a2fc474aab61ee72c8fb
Darien Nurse <darienurse@gmail.com>
2018-04-24 16:58:40 -0400

    add unit test

---
+++ app/src/test/java/com/timer/nurse/iheartbutton2/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.timer.nurse.iheartbutton2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getTimestamp() {
+    }
+}
